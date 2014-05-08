8ddd828de736790e8d6c906149ef999ee706861a
Erik <erik.chaupis@verizon.com>
2014-05-08 14:26:09 -0500

    first version

---
+++ ant/ant-project-sample/ReadMe.md
@@ -0,0 +1,6 @@
+
+Debug from command line:
+--------------------------------------
+-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9009
+
+ <jvmarg line="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=9009" />
---
+++ ant/ant-project-sample/src/com/ant/core/Process.java
@@ -0,0 +1,19 @@
+package com.ant.core;
+
+public class Process {
+
+	public static void main(String[] args) {
+
+		System.out.println("init");
+		System.out.println(2);
+		int a=5;
+		a+=10;
+		a+=30;
+		System.out.println(a);
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello World: "+i);
+		}
+
+	}
+}
